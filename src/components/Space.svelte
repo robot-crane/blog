ab8b17f3682773dc1be4ace00d57cddebfe03cdf
crane <paper_robot@outlook.com>
2024-12-11 00:04:43 +0800

    space

---
+++ src/components/Space.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import clsx from "clsx";
+
+  type Align = "center" | "start" | "end";
+  export let align: Align = "center";
+
+  export let gap: number | string = 1;
+
+  const rowClassMap = {
+    center: "justify-center",
+    start: "justify-start",
+    end: "justify-end",
+  };
+
+  let rowClass = rowClassMap[align];
+  let gapClass =
+    typeof gap === "string" && gap.indexOf("px") > 0
+      ? `gap-x-[${gap}]`
+      : `gap-x-${gap}`;
+</script>
+
+<div class={clsx("w-full flex", rowClass, gapClass)}>
+  <slot />
+</div>
+
+<style></style>
--- src/components/Tag.svelte
+++ src/components/Tag.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  export let text = "";
 </script>
 
 <span class="px-1 py-0.5 box-border text-xs border rounded"><slot></slot></span>
